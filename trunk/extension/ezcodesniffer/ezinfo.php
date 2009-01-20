3bc43f586097c09c653a7eec45ab4e81e7de448b
Gaetano Giunta <gg@ez.no>
2009-01-20 15:46:12 +0000

    - add license, info file
    git-svn-id: http://svn.projects.ez.no/ezcodensiffer@7 5ffef432-9b8c-415e-80f7-4c6a55867b54

---
+++ trunk/extension/ezcodesniffer/ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+class ezcodesnifferInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "ezcodesniffer",
+            'Version' => "0.1",
+            'Copyright' => "Copyright (C) 2009 G. Giunta",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
