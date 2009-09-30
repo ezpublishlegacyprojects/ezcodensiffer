2c76fb9ddf5708f14ec141ea6d998421af738c6a
llaumgui <info@example.com>
2009-09-30 08:26:57 +0000

    First test
    git-svn-id: http://svn.projects.ez.no/ezcodensiffer@9 5ffef432-9b8c-415e-80f7-4c6a55867b54

---
+++ branches/sandboxes/llaumgui/eZPublish/Sniffs/ControlStructures/ControlSignatureSniff.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Verifies that control statements conform to their coding standards.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Guillaume Kulakowski <guillaume@llaumgui.com>
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+if (class_exists('PHP_CodeSniffer_Standards_AbstractPatternSniff', true) === false) {
+    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_AbstractPatternSniff not found');
+}
+
+/**
+ * Verifies that control statements conform to their coding standards.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Guillaume Kulakowski <guillaume@llaumgui.com>
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: 1.2.0
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class eZPublish_Sniffs_ControlStructures_ControlSignatureSniff extends PHP_CodeSniffer_Standards_AbstractPatternSniff
+{
+
+
+    /**
+     * Constructs a eZPublish_Sniffs_ControlStructures_ControlSignatureSniff.
+     */
+    public function __construct()
+    {
+        parent::__construct(true);
+
+    }//end __construct()
+
+
+    /**
+     * Returns the patterns that this test wishes to verify.
+     *
+     * @return array(string)
+     *
+     * @TODO Better
+     */
+    protected function getPatterns()
+    {
+        return array(
+                'doEOL...{EOL...}EOL...while (...);EOL',
+                'while (...)EOL...{EOL',
+                'for (...)EOL...{EOL',
+                'if (...)EOL...{EOL',
+                'foreach (...)EOL...{EOL',
+                '}EOL...else if (...)EOL...{EOL',
+                '}EOL...elseif (...)EOL...{EOL',
+                '}EOL...elseEOL{...EOL',
+                'doEOL...{EOL',
+               );
+
+    }//end getPatterns()
+
+
+}//end class
+
+?>
---
+++ branches/sandboxes/llaumgui/eZPublish/eZPublishCodingStandard.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * eZPublish Coding Standard.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Guillaume Kulakowski <guillaume@llaumgui.com>
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+if (class_exists('PHP_CodeSniffer_Standards_CodingStandard', true) === false) {
+    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_CodingStandard not found');
+}
+
+/**
+ * eZPublish Coding Standard.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Guillaume Kulakowski <guillaume@llaumgui.com>
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: 1.2.0
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PHP_CodeSniffer_Standards_eZPublish_eZPublishCodingStandard extends PHP_CodeSniffer_Standards_CodingStandard
+{
+
+
+    /**
+     * Return a list of external sniffs to include with this standard.
+     *
+     * The eZPublish standard uses some generic and PEAR sniffs.
+     *
+     * @return array
+     */
+    public function getIncludedSniffs()
+    {
+        return array(
+                'PEAR',
+                'Generic/Sniffs/NamingConventions/UpperCaseConstantNameSniff.php',
+                'Generic/Sniffs/PHP/LowerCaseConstantSniff.php',
+                'Generic/Sniffs/PHP/DisallowShortOpenTagSniff.php',
+                'Generic/Sniffs/WhiteSpace/DisallowTabIndentSniff.php',
+               );
+
+    }//end getIncludedSniffs()
+
+
+    /**
+     * Return a list of external sniffs to exclude with this standard.
+     *
+     * @return array
+     */
+    public function getExcludedSniffs()
+    {
+        return array(
+                'PEAR/Sniffs/ControlStructures',
+                'PEAR/Sniffs/Commenting/ClassCommentSniff.php',
+                'PEAR/Sniffs/Commenting/FileCommentSniff.php',
+                'PEAR/Sniffs/Commenting/FunctionCommentSniff.php',
+                'PEAR/Sniffs/Files/LineLengthSniff.php',
+                'PEAR/Sniffs/Functions/FunctionCallSignatureSniff.php',
+                'PEAR/Sniffs/NamingConventions/ValidClassNameSniff.php'
+               );
+
+    }//end getExcludedSniffs()
+
+
+}//end class
+
+?>
