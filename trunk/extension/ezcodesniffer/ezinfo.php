6e1fdc39473ae82fa9a7bee1952b8140d20a5d3b
Gaetano Giunta <gg@ez.no>
2010-02-19 16:42:11 +0000

    - prepare for integration with ezextensionbuilder
    git-svn-id: http://svn.projects.ez.no/ezcodensiffer@13 5ffef432-9b8c-415e-80f7-4c6a55867b54

--- trunk/extension/ezcodesniffer/ezinfo.php
+++ trunk/extension/ezcodesniffer/ezinfo.php
@@ -5,7 +5,7 @@ class ezcodesnifferInfo
     {
         return array(
             'Name' => "ezcodesniffer",
-            'Version' => "0.1",
+            'Version' => '0.1',
             'Copyright' => "Copyright (C) 2009 G. Giunta",
             'License' => "GNU General Public License v2.0"
         );
